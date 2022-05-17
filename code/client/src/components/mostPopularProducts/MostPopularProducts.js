23290c0909657c7326239d45299c637d6671c0c1
hoangtuan2000 <duonghoangtuan.td@gmail.com>
2022-05-17 09:29:26 +0700

    done version 1

--- code/client/src/components/mostPopularProducts/MostPopularProducts.js
+++ code/client/src/components/mostPopularProducts/MostPopularProducts.js
@@ -36,50 +36,52 @@ function MostPopularProducts(props) {
               {
                 products.map((product) => {
                   return (
-                    <div className='p-2 pb-3 float-start cardProduct' key={product.id_sp} style={{ width: '220px' }}>
-                      <Link to={`/product-detail/${product.id_sp}`}>
-                        <Card >
-                          <Card.Img variant="top"
-                            src={URL + product.anh_sp}
-                            className='mt-2'
-                          />
-                          <Card.Body className='p-2' >
-                            {
-                              product.giam_km > 0 ?
-                                <Badge bg="danger"> Giảm {product.giam_km} % </Badge>
-                                :
-                                <br />
-                            }
-                            <Card.Title>
-                              <OverlayTrigger
-                                placement='top'
-                                overlay={
-                                  <Tooltip>
-                                    {product.ten_sp}
-                                  </Tooltip>
-                                }
-                              >
-                                <span>
-                                  {nameProductFormat(product.ten_sp, 18)}
-                                </span>
-                              </OverlayTrigger>
-                            </Card.Title>
-                            {
-                              product.giam_km > 0 ?
-                                <>
-                                  <h6>{moneyFormat(product.gia_sp)} VNĐ</h6>
-                                  <h5>{moneyFormat(reducedPrice(product.gia_sp, product.giam_km))} VNĐ</h5>
-                                </>
-                                :
-                                <>
-                                  <h6><br /></h6>
-                                  <h5>{moneyFormat(reducedPrice(product.gia_sp, product.giam_km))} VNĐ</h5>
-                                </>
-                            }
-                          </Card.Body>
-                        </Card>
-                      </Link>
-                    </div>
+                    <>
+                      <div className='p-1 pb-3 float-start cardProduct' key={product.id_sp} style={{ width: '220px' }}>
+                        <Link to={`/product-detail/${product.id_sp}`}>
+                          <Card >
+                            <Card.Img variant="top"
+                              src={URL + product.anh_sp}
+                              className='mt-2'
+                            />
+                            <Card.Body className='p-2' >
+                              {
+                                product.giam_km > 0 ?
+                                  <Badge bg="danger"> Giảm {product.giam_km} % </Badge>
+                                  :
+                                  <br />
+                              }
+                              <Card.Title>
+                                <OverlayTrigger
+                                  placement='top'
+                                  overlay={
+                                    <Tooltip>
+                                      {product.ten_sp}
+                                    </Tooltip>
+                                  }
+                                >
+                                  <span>
+                                    {nameProductFormat(product.ten_sp, 18)}
+                                  </span>
+                                </OverlayTrigger>
+                              </Card.Title>
+                              {
+                                product.giam_km > 0 ?
+                                  <>
+                                    <h6>{moneyFormat(product.gia_sp)} VNĐ</h6>
+                                    <h5>{moneyFormat(reducedPrice(product.gia_sp, product.giam_km))} VNĐ</h5>
+                                  </>
+                                  :
+                                  <>
+                                    <h6><br /></h6>
+                                    <h5>{moneyFormat(reducedPrice(product.gia_sp, product.giam_km))} VNĐ</h5>
+                                  </>
+                              }
+                            </Card.Body>
+                          </Card>
+                        </Link>
+                      </div>
+                    </>
                   )
                 })
               }
